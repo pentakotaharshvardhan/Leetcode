166b01f663bdc748f3eb3ba704b2b566bf23d865
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-03-12 17:30:35 +0530

    Time: 618 ms (5.03%) | Memory: 50.5 MB (86.07%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        double[] distance=new double[points.length];
+        int n=points.length;
+        double[] distance=new double[n];
         int[][] ans=new int[k][2];
-        for(int i=0;i<points.length;i++){
+        for(int i=0;i<n;i++){
             distance[i]=Math.sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
         }
-        // double[] sample=new double[points.length];
-        // for(int i=0;i<sample.length;i++){
-        //     sample[i]=distance[i];
-        // }
-        // int m=0;
-        // Arrays.sort(distance);
-        //System.out.println(Arrays.toString(sample));
-        //System.out.println(Arrays.toString(distance));
-        boolean[] istrue=new boolean[distance.length];
+        boolean[] istrue=new boolean[n];
         for(int j=0;j<k;j++){
             int idx=0;
             double dis=10000000;
-            for(int i=0;i<distance.length;i++){
+            for(int i=0;i<n;i++){
                 if(distance[i]<dis && !istrue[i]) {
                     idx=i;
                     dis=distance[i];
