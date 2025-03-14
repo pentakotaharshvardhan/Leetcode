b4176fbf75d9fd209394f95fc3d5ac5b2da3deab
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-03-14 13:05:45 +0530

    Time: 38 ms (21.24%) | Memory: 63.6 MB (7.93%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -2,14 +2,15 @@ class Solution {
     public int maximumCandies(int[] candies, long k) {
         Arrays.sort(candies);
         int max=Integer.MIN_VALUE;
-        int sum=0;
+        long sum=0;
         for(int i:candies){
             max=Math.max(max,i);
+            sum+=i;
         }
         //if(sum<k) return 0;
-        int start=1;
-        int end=max;
-        int  mid=0;
+        long start=1;
+        long end=sum/k;
+        long  mid=0;
         //boolean istrue=false;
         while(start<=end){
             mid=start+(end-start)/2;
@@ -22,6 +23,6 @@ class Solution {
         }
         //if(!istrue) return 0;
         //System.out.println(end);
-        return end;
+        return (int)end;
     }
 }
