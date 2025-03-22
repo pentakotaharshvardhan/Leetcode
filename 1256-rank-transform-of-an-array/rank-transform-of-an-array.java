4874db4235ff59a84fdd7c6f2960945f8f8511db
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-03-22 13:06:09 +0530

    Time: 33 ms (41.22%) | Memory: 60.3 MB (91.77%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] temp=arr.clone();
+        Arrays.sort(temp);
+        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+        int count=1;
+        for(int i=0;i<temp.length;i++){
+            if(!hm.containsKey(temp[i])){
+                if(i>0 && temp[i]!=temp[i-1]){
+                    hm.put(temp[i],++count);
+                }
+                else hm.put(temp[i],count);
+            }
+        }
+        //System.out.println(Arrays.toString(arr)+" "+Arrays.toString(temp));
+        for(int i=0;i<arr.length;i++){
+            if(hm.containsKey(arr[i])){
+                arr[i]=hm.get(arr[i]);
+            }
+        }
+        return arr;
+    }
+}
