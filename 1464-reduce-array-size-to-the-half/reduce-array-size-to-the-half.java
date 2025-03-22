674964a54f52a057f15f656d4bc0a6f9fa6c82a8
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-03-22 13:34:59 +0530

    Time: 59 ms (5.41%) | Memory: 55.3 MB (96.00%) - LeetSync

---
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        int ans=0;
+        int m=arr.length;
+        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+        for(int i=0;i<arr.length;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+        //System.out.println(hm);
+        PriorityQueue<Integer> pr=new PriorityQueue<>(new Comparator<>(){
+            @Override
+            public int compare(Integer a,Integer b){
+                if(a<b) return 1;
+                else  return -1;
+            }
+        });
+        for(int x:hm.keySet()){
+            pr.add(hm.get(x));
+        }
+        //System.out.println(pr);
+        while(!pr.isEmpty()){
+            int n=pr.poll();
+            m-=n;
+           // System.out.println(n+" "+m);
+            ans++;
+            if(m<=arr.length/2) return ans;
+        }
+        return ans;
+    }
+}
