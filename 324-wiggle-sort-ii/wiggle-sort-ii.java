5cb4a63d06c31803f32d12bc800b4d8365e6fe16
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-03-23 07:55:18 +0530

    Time: 7 ms (30.23%) | Memory: 48.1 MB (15.80%) - LeetSync

---
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int[] temp=new int[nums.length];
+        Arrays.sort(nums);
+        int j=0;
+        for(int i=1;i<nums.length;i+=2){
+            temp[i]=nums[nums.length-1-j++];
+        }
+        for(int i=0;i<nums.length;i+=2){
+            if(temp[i]==0) temp[i]=nums[nums.length-1-j++];
+        }
+        //System.out.println(Arrays.toString(temp));
+        for(int m=0;m<temp.length;m++){
+            nums[m]=temp[m];
+        }
+    }
+}
