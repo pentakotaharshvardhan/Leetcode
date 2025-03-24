831c8a1cc36a3a0c7e8da1a69ae83314201f19f5
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-03-24 09:52:43 +0530

    Time: 101 ms (18.85%) | Memory: 57.1 MB (92.67%) - LeetSync

---
+++ 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] getStrongest(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] ans=new int[k];
+        int mid=(arr.length-1)/2;
+        int val=arr[mid];
+        Integer[] arr1=new Integer[arr.length];
+        int j=0;
+        for(int x:arr){
+            arr1[j++]=x;
+        }
+        Arrays.sort(arr1, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                int val1 = Math.abs(a - val);
+                int val2 = Math.abs(b - val);
+                if (val1 < val2) return 1;
+                else if (val1 > val2) return -1;
+                else return Integer.compare(b, a); 
+            }
+        });
+
+        System.out.println(Arrays.toString(arr1));
+        for(int i=0;i<k;i++){
+            ans[i]=arr1[i];
+        }
+        return ans;
+    }
+}
