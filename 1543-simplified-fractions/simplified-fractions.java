a1ff1e3e0896ae3fc5186bee7c9487a50494ab89
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-04-04 15:33:50 +0530

    Time: 18 ms (83.62%) | Memory: 46 MB (34.05%) - LeetSync

--- 1543-simplified-fractions/simplified-fractions.java
+++ 1543-simplified-fractions/simplified-fractions.java
@@ -1,5 +1,10 @@
 class Solution {
-    public List<String> simplifiedFractions(int n) {
+    static{
+        for(int i=0;i<500;i++){
+            simplifiedFractions(0);
+        }
+    }
+    public static List<String> simplifiedFractions(int n) {
         List<String> ans=new ArrayList<>();
         for(int i=1;i<n;i++){
             for(int j=i+1;j<=n;j++){
