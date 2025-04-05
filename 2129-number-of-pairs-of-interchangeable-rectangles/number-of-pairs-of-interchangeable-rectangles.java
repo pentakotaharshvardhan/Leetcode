67a5c088a6f54ebf579a8ac4e42e70a6eb6c6b3d
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-04-05 14:09:15 +0530

    Time: 732 ms (5.13%) | Memory: 108.3 MB (5.36%) - LeetSync

---
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long interchangeableRectangles(int[][] rectangles) {
+        long ans=0;
+        HashMap<Double,Long> hm=new HashMap<>();
+        for(int i=0;i<rectangles.length;i++){
+            int num=rectangles[i][0];
+            int den=rectangles[i][1];
+            double frac=den*1.0/num;
+            System.out.println(frac);
+            hm.put(frac,hm.getOrDefault(frac,(long)0)+1);
+        }
+        for(double n:hm.keySet()){
+            long k=hm.get(n)-1;
+            ans+=(k*(k+1))/(long)2;
+        }
+        //System.out.println(hm);
+        return ans;
+    }
+}
