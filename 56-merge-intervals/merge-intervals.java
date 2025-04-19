260d2c030d5fb29f28b5905d1139db1fc744d8b6
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-04-19 17:37:14 +0530

    Time: 4 ms (98.63%) | Memory: 46.9 MB (16.17%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<Integer> list=new ArrayList<>();
+        int[] arr=new int[intervals.length];
+        int[] arr2=new int[intervals.length];
+        for(int i=0;i<intervals.length;i++){
+            arr[i]=intervals[i][0];
+            arr2[i]=intervals[i][1];
+        }
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+        int i=0,j=0,start=arr[0],end=arr2[0];
+        while(i<arr.length &&j<arr.length){
+            if( arr[i]<=arr2[j]){
+                i++;
+            }
+            if(i<arr.length &&arr[i]<=arr2[j]){
+                j=i;
+                end=arr2[j];
+            }
+            else{
+                list.add(start);
+                list.add(end);
+                if(i>=arr.length){
+                    break;
+                }
+                else {
+                    j = j + 1;
+                    start = arr[i];
+                    end = arr2[j];
+                }
+            }
+        }
+        int[][] arr3=new int[list.size()/2][2];
+        int m=0;
+        for(int k=0;k<list.size()/2;k++){
+            arr3[k][0]=(int)list.get(m++);
+            arr3[k][1]=(int)list.get(m++);
+        }
+        return arr3;
+    }
+}
