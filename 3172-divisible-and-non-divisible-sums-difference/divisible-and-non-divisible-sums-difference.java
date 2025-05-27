313ae5f2a57dd788e2a3dc8feae1dcfb75f0f7e0
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-05-27 09:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (82.55%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -5,7 +5,7 @@ class Solution {
         for(int j=1;j<=i;j++){
             sum+=m*j;
         }
-        System.out.println(sum);
+        //System.out.println(sum);
         int s=(n*(n+1)/2)-sum;
         return s-sum;
     }
