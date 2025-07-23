41a4202cd586005357f1d53cfc9b094a3c395825
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-07-23 09:29:45 +0530

    Time: 40 ms (66.81%) | Memory: 76 MB (44.71%) - LeetSync

---
+++ 456-132-pattern/132-pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        if (nums.length < 3) return false;
+        Stack<Integer> stack = new Stack<>();
+        int third = Integer.MIN_VALUE; 
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < third) return true;
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                third = stack.pop();
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
