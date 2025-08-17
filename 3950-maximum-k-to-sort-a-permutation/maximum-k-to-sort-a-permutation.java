3a1f41f52b41eb23b4e0cc6151f6c1c4154a245b
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-08-17 10:31:40 +0530

    Time: 13 ms (13.87%) | Memory: 61.4 MB (86.37%) - LeetSync

--- 3950-maximum-k-to-sort-a-permutation/maximum-k-to-sort-a-permutation.java
+++ 3950-maximum-k-to-sort-a-permutation/maximum-k-to-sort-a-permutation.java
@@ -1,19 +1,44 @@
 class Solution {
-    public int sortPermutation(int[] arr) {
-        int n=arr.length;
-        List<Integer> list=new ArrayList<>();
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]!=i){
-                list.add(arr[i]);
-            }
+    public int sortPermutation(int[] nums) {
+        int ans=Integer.MAX_VALUE;
+        boolean isdesc=true;
+        boolean isasc=true;
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]<nums[i]) isdesc=false;
+            if(nums[i-1]>nums[i]) isasc=false;
+            if(max<nums[i]) max=nums[i];
         }
-        System.out.println(list);
-        if(list.size()==0)return 0;
-        int ans=list.get(0);
-        for(int i=1;i<list.size();i++){
-            ans=ans&list.get(i);
+        if(isdesc) return 0;
+        if(isasc) return 0;
+        int[] ixd=new int[max+1];
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[i];
+            ixd[nums[i]]=i;
         }
+        Arrays.sort(arr);
+        for(int i=nums.length-1;i>=0;i--){
+            if(ixd[arr[i]]==i) continue;
+            ans=ans & arr[i]&nums[i];
+            ixd[nums[i]]=ixd[arr[i]];
+            nums[ixd[arr[i]]]=nums[i];
+            //System.out.println(arr[i]+" "+nums[i]+" "+ans);
+        }
+        // for(int i=0;i<nums.length;i++){
+        //     ixd[nums[i]]=i;
+        // }
+        // //System.out.println("***");
+        // int ans2=ans;
+        // for(int i=0;i<nums.length;i++){
+        //     if(ixd[arr[i]]==i) continue;
+        //     int temp=arr[i]&nums[i];
+        //     ixd[nums[i]]=ixd[arr[i]];
+        //     nums[ixd[arr[i]]]=nums[i];
+        //     if(temp<ans) ans=temp;
+        //     //System.out.println(arr[i]+" "+nums[i]+" "+ans);
+        // }
+        // if(ans2<ans) return ans2;
         return ans;
     }
 }
