3390d6587cae6a7e95f29e9968009ae7de564104
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-12-08 18:30:24 +0530

    Time: 2 ms (57.39%) | Memory: 42.8 MB (82.00%) - LeetSync

---
+++ 4054-count-distinct-integers-after-removing-zeros/count-distinct-integers-after-removing-zeros.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long countDistinct(long n) {
+
+        String s=Long.toString(n);
+        int len=s.length();
+        long ans=0;
+        long pow=9;
+        for(int i=1;i<len;i++){
+            ans+=pow;
+            pow*=9;
+        }
+
+        for(int i=0;i<len;i++){
+            if((s.charAt(i)-'0')==0){
+                break;
+            }
+            long choices=(s.charAt(i)-'0')-1;
+            long m=1;
+            for(int j=i+1;j<len;j++){
+                m*=9;
+            }
+            ans+=(choices*m);
+        }
+        if(!s.contains("0")){
+            ans++;
+        }
+        return ans;
+    }
+    
+}
