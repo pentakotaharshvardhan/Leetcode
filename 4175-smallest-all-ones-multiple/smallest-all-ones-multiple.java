3d10c0b9eb837538a474d11aac9ff7c9466f02df
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2025-12-28 22:29:45 +0530

    Time: 8 ms (100.00%) | Memory: 42.6 MB (100.00%) - LeetSync

---
+++ 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minAllOneMultiple(int k) {
+        if(k%2==0 || k%5==0) return -1;
+        int t1=0;
+        for(int i=1;i<=k;i++){
+            t1=(t1*10+1)%k;
+            if(t1==0) return i;
+        }
+        return -1;
+    }
+}
