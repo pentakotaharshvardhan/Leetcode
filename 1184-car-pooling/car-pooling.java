163bda96b908d1053acdfc0d3a667b7a88da5a35
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2026-03-28 11:45:00 +0530

    Time: 6 ms (37.47%) | Memory: 46.3 MB (72.43%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,36 +1,19 @@
-import java.util.*;
-
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        Arrays.sort(trips, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return Integer.compare(a[1], b[1]);
-            }
-        });
-        PriorityQueue<Integer> pr = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                return Integer.compare(trips[a][2], trips[b][2]);
-            }
-        });
-
-        int cap = trips[0][0]; 
-        int end = trips[0][1];
-        if(cap>capacity) return false;
-        pr.add(0); 
-
-        for (int i = 1; i < trips.length; i++) {
-            while (!pr.isEmpty() && trips[pr.peek()][2] <= trips[i][1]) {
-                cap -= trips[pr.poll()][0]; 
-            }
-
-            pr.add(i);
-            cap += trips[i][0];
-
-            end = trips[i][2]; 
-
-            if (cap > capacity) return false;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<trips.length;i++){
+            max=Math.max(trips[i][2],max);
+        }
+        int[] arr=new int[max+2];
+        for(int i=0;i<trips.length;i++){
+            arr[trips[i][1]]+=trips[i][0];
+            arr[trips[i][2]]-=trips[i][0];
+        }
+        System.out.println(Arrays.toString(arr));
+        if(arr[0]>capacity) return false;
+        for(int i=1;i<arr.length;i++){
+            arr[i]+=arr[i-1];
+            if(arr[i]>capacity) return false;
         }
         return true;
     }
