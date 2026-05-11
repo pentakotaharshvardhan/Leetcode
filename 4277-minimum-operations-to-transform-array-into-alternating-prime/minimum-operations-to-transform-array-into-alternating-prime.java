5252d2f091abeab11300431ea9f4bd36ef2dd519
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2026-05-11 11:43:58 +0530

    Time: 84 ms (90.10%) | Memory: 92.7 MB (79.26%) - LeetSync

--- 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.java
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.java
@@ -1,40 +1,64 @@
 class Solution {
-    public static List<Integer> getPrime(int n) {
+
+    private static final int LIMIT = 150000;
+    private static final List<Integer> prime = getPrime(LIMIT);
+
+    private static List<Integer> getPrime(int n) {
+        boolean[] composite = new boolean[n + 1];
         List<Integer> list = new ArrayList<>();
-        boolean[] visi = new boolean[n + 1];
+
         for (int i = 2; i <= n; i++) {
-            if (!visi[i]) {
+            if (!composite[i]) {
                 list.add(i);
-                for (long j = (long) i * i; j <= n; j += i) visi[(int) j] = true;
+
+                if ((long) i * i <= n) {
+                    for (long j = (long) i * i; j <= n; j += i) {
+                        composite[(int) j] = true;
+                    }
+                }
             }
         }
+
         return list;
     }
-    public List<Integer> prime=getPrime(150000);
+
     public int minOperations(int[] nums) {
-        //System.out.println(prime);
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            int t1=binarySearch(prime,nums[i]);
-            if(i%2!=0 ){
-                if(nums[i]==2) ans+=2;
-                else if(!(nums[i]!=t1))ans+=1;
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int nextPrime = lowerBoundPrime(nums[i]);
+
+            if (i % 2 != 0) {
+                if (nums[i] == 2) {
+                    ans += 2;
+                } else if (nums[i] == nextPrime) {
+                    ans += 1;
+                }
                 continue;
             }
-            //System.out.println(nums[i]+" "+t1);
-            ans+=(t1-nums[i]);
+
+            ans += nextPrime - nums[i];
         }
+
         return ans;
     }
-    public int binarySearch(List<Integer> prime,int target){
-        int start=0;
-        int end=prime.size();
-        while(start<=end){
-            int mid=end-(end-start)/2;
-            if(prime.get(mid)==target) return prime.get(mid);
-            else if(prime.get(mid)>target) end=mid-1;
-            else start=mid+1;
+
+    private int lowerBoundPrime(int target) {
+        int start = 0;
+        int end = prime.size() - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (prime.get(mid) >= target) {
+                ans = prime.get(mid);
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
         }
-        return prime.get(start);
+
+        return ans;
     }
 }
