f6a3a599bce4dc7c7a9c75ba045557cb3b8d10d8
Harshvardhan Pentakota <167344115+pentakotaharshvardhan@users.noreply.github.com>
2026-09-08 15:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (57.08%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        int temp=n;
+        int digit=0;
+        int ans=0;
+        ans=n-999;
+        if(ans<=0) return 0;
+        return ans;
+        
+    }
+}
